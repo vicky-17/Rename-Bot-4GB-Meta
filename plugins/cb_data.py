dde0736139eb489a93facb54dad75f736d9c2cee
vicky-17 <debashishkuanr17@gmail.com>
2026-02-23 19:19:51 +0530

    ai lang detection algorithm change

--- plugins/cb_data.py
+++ plugins/cb_data.py
@@ -2,7 +2,7 @@
 import os
 from helper.progress import progress_for_pyrogram
 from pyrogram import Client, filters
-from pyrogram.types import (InlineKeyboardButton, InlineKeyboardMarkup, ForceReply)
+from pyrogram.types import (InlineKeyboardButton, InlineKeyboardMarkup, ForceReply, CallbackQuery)
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
 from helper.database import *
